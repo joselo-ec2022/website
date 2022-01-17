b02889708fd875b77b389c9af2dce5b32d897c87
Joselo <joselomanzano@hotmail.com>
2022-01-16 19:25:55 -0500

    añadiendo un switch

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
 echo "bienvenido";
+$num1 = 21;
 $num = 2;
 
-
+switch($num1 > 2){
+case 21:
 if($num == 2){
 echo "numero es igual a 2";
 }else{
     echo "numero tiene un valor diferente";
 }
-
+break;
+}
 
 
 ?>
