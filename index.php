969f0be4120c369710c8fb4e609adfb9f66ef234
Joselo <joselomanzano@hotmail.com>
2022-01-16 19:34:42 -0500

    añadir un for anidado

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <?php
 
 echo "bienvenido";
+$do = 12;
+$nombre = "jose";
 $num1 = 21;
 $num = 2;
 
-switch($num1 > 2){
-case 21:
-if($num == 2){
-echo "numero es igual a 2";
-}else{
-    echo "numero tiene un valor diferente";
+
+do{
+for($i=1; $i<=5; $i++){
+
+echo $i + " " ;
+
 }
+} while($do > 11);
+
+while($nombre == "jose"){
+
+switch($num1 > 2){
+
+    case 21:
+
+        if($num == 2){
+
+            echo "numero es igual a 2";
+
+        }else{
+
+            echo "numero tiene un valor diferente";
+
+        }
 break;
 }
 
-
+}
 ?>
