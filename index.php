d625f18fd3a7f17fb6de73a614f33f4cfa1f0458
Joselo <joselomanzano@hotmail.com>
2022-01-16 19:39:24 -0500

    Merge branch 'master' of https://github.com/joselo-ec2022/website

--- index.php
+++ index.php
@@ -1,39 +1,17 @@
 <?php
 
 echo "bienvenido";
-$do = 12;
-$nombre = "jose";
 $num1 = 21;
 $num = 2;
 
-
-do{
-for($i=1; $i<=5; $i++){
-
-echo $i + " " ;
-
-}
-} while($do > 11);
-
-while($nombre == "jose"){
-
 switch($num1 > 2){
-
-    case 21:
-
-        if($num == 2){
-
-            echo "numero es igual a 2";
-
-        }else{
-
-            echo "numero tiene un valor diferente";
-
-        }
+case 21:
+if($num == 2){
+echo "numero es igual a 2";
+}else{
+    echo "numero tiene un valor diferente";
+}
 break;
 }
 
-?>
-
-}
 ?>
